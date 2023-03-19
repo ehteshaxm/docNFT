0f26245784d7db8dc664dfb99a8c973615bbbd8f
Ehtesham Siddiqui <ehteshamsiddiqui@Ehteshams-MacBook-Pro.local>
2023-03-19 15:24:24 +0530

    UI

--- pages/index.js
+++ pages/index.js
@@ -314,23 +314,23 @@ const index = () => {
     <>
       {aadhaarData === null && licenseData === null ? (
         <div className='min-h-screen bg-slate-50 flex justify-center items-center flex-col relative'>
-          <nav class='bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600'>
+          <nav class='bg-white px-2 sm:px-4 py-2.5  fixed w-full z-20 top-0 left-0 border-b border-gray-200'>
             <div class='container flex flex-wrap items-center justify-between mx-auto'>
-              <span class='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
+              <span class='self-center text-xl font-semibold whitespace-nowrap'>
                 🖼️ docNFT-KYC
               </span>
 
               <div class='flex md:order-2'>
                 <button
                   type='button'
-                  class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                  class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 :bg-blue-600 :hover:bg-blue-700 :focus:ring-blue-800'
                 >
                   Get started
                 </button>
                 <button
                   data-collapse-toggle='navbar-sticky'
                   type='button'
-                  class='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+                  class='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 :text-gray-400 :hover:bg-gray-700 :focus:ring-gray-600'
                   aria-controls='navbar-sticky'
                   aria-expanded='false'
                 >
@@ -354,11 +354,11 @@ const index = () => {
                 class='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
                 id='navbar-sticky'
               >
-                <ul class='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+                <ul class='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white :bg-gray-800 md::bg-gray-900 :border-gray-700'>
                   <li>
                     <a
                       href='#'
-                      class='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
+                      class='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 :text-white'
                       aria-current='page'
                     >
                       Home
@@ -367,7 +367,7 @@ const index = () => {
                   <li>
                     <a
                       href='#'
-                      class='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                      class='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md::hover:text-white :text-gray-400 :hover:bg-gray-700 :hover:text-white md::hover:bg-transparent :border-gray-700'
                     >
                       About
                     </a>
@@ -375,7 +375,7 @@ const index = () => {
                   <li>
                     <a
                       href='#'
-                      class='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                      class='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md::hover:text-white :text-gray-400 :hover:bg-gray-700 :hover:text-white md::hover:bg-transparent :border-gray-700'
                     >
                       NFTs
                     </a>
@@ -383,7 +383,7 @@ const index = () => {
                   <li>
                     <a
                       href='#'
-                      class='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                      class='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md::hover:text-white :text-gray-400 :hover:bg-gray-700 :hover:text-white md::hover:bg-transparent :border-gray-700'
                     >
                       Connect
                     </a>
